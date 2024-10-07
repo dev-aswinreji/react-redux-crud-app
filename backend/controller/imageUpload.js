9ea924d00fa246fc248939c6e08f518e30acb1f5
Aswin Reji <aswinreji.004@gmail.com>
2024-10-07 12:47:07 +0000

    fix: Refactored code

--- backend/controller/imageUpload.js
+++ backend/controller/imageUpload.js
@@ -6,15 +6,9 @@ export const imageUpload = async (req, res) => {
         console.log(req.body, 'file is here');
         const { fileName } = req.body
         const userid = req.user?.userid
-        // const userData = await pool.query(`SELECT email from users 
-        // WHERE userid='${userid}'
-        // `)
-        // console.log(userData.rows[0]?.email, 'userData is here');
         const output = await pool.query(`UPDATE users SET imageurl=$1 WHERE userid=$2`,
             [fileName, userid]
         )
-        console.log(output, 'output is showing');
-
         res.status(201).json({ message: "Image uploaded success" ,fileUrl:output.rows[0]?.imageurl})
     } catch (error) {
         console.log(error, 'Error is showing while image Upload');
